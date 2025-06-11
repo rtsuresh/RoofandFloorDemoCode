eff2fae64a56d61e44f310eff5156818f8f24a91
rtsuresh <rtsuresh.87@gmail.com>
2025-06-12 09:46:46 +1000

    Update Variables.java

--- Project-RoofandFloor/src/supportingFiles/Variables.java
+++ Project-RoofandFloor/src/supportingFiles/Variables.java
@@ -6,7 +6,6 @@ public class Variables {
 			// Timeout- Generic (in seconds)
 		public static final int intTimeOut = 10;
 		public static final String strLocator = "XPATH";
-		public static final String POSTMAN_API_KEY = "PMAK-6849080a9c59fa00011e1440-0d810ba0c39206cb7213487ef192b026c1";
 		
 		// Variables used in Script
 		public static WebDriver driver;
